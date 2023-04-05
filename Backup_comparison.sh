2934971e94e399f41d24e2d35ac8aae9269b5335
Eyyuep <115067739+Eyyuep@users.noreply.github.com>
2023-04-06 01:27:06 +0200

    Update Backup_comparison.sh
    Methode check_missing_files() wurde erstellt um den Anwender die Möglichkeit zu geben mit CMD-Befehl "nano backup_check_result.txt" Einblick darüber zu geben welche Datei tatsächlich fehlen. 
    
    Mit anderen worten: check_missing_files() ist eine Funktion, die prüft, ob alle Dateien im Backup-Ordner auf dem entfernten Server auch im Download-Ordner auf dem lokalen Client vorhanden sind. Wenn eine Datei im Backup-Ordner fehlt, wird eine Warnung ausgegeben. Diese Funktion hilft Benutzern dabei, sicherzustellen, dass alle notwendigen Dateien vom entfernten Server erfolgreich heruntergeladen wurden und dass keine wichtigen Dateien fehlen.

--- Backup_comparison.sh
+++ Backup_comparison.sh
@@ -5,6 +5,15 @@ REMOTE_HOST="remotehost@192.168.41.131"
 BACKUP_FOLDER="/home/remotehost/Backup-Ordner"
 DOWNLOAD_FOLDER="/home/baran/Download-Ordner"
 
+# Funktion für den SSH-Befehlsblock
+check_missing_files() {
+  ssh -n "$REMOTE_HOST" "cd \"$BACKUP_FOLDER\" && find . -type f" | while read file; do
+    if [ ! -f "$DOWNLOAD_FOLDER/$file" ]; then
+      echo "WARN: $file fehlt im Download-Ordner" >> backup_check_result.txt
+    fi
+  done
+}
+
 # Verbindet sich mit dem entfernten Server per SSH und zählt die Anzahl der Dateien im Backup-Ordner
 backup_count=$(ssh -n "$REMOTE_HOST" "ls -1 \"$BACKUP_FOLDER\" | wc -l")
 
@@ -17,6 +26,7 @@ if [ "$backup_count" -ne 0 ]; then
 else
     echo "echo \"<<<compare_files>>>\"" > backup_check_result.txt
     echo "1 - Der Backup-Ordner auf dem entfernten Server ist leer" >> backup_check_result.txt
+    check_missing_files
     exit 1
 fi
 
@@ -24,13 +34,16 @@ fi
 if [ "$backup_count" -eq "$download_count" ]; then
     echo "echo \"<<<compare_files>>>\"" > backup_check_result.txt
     echo "2 - Anzahl der Dateien im Download-Ordner entspricht der Anzahl im Backup-Ordner ($backup_count von $backup_count)" >> backup_check_result.txt
+    check_missing_files
     exit 2
 elif (( $(echo "$match_percent >= 50 && $match_percent < 99" | bc -l) )); then
     echo "echo \"<<<compare_files>>>\"" > backup_check_result.txt
     echo "3 - Anzahl der Dateien im Download-Ordner entspricht nicht der Anzahl im Backup-Ordner ($download_count von $backup_count, $match_percent% Übereinstimmung)" >> backup_check_result.txt
+    check_missing_files
     exit 3
 else
     echo "echo \"<<<compare_files>>>\"" > backup_check_result.txt
     echo "4 - Es Befinden sich nur $download_count von $backup_count Datein im Downloadordner ($match_percent% Übereinstimmung)" >> backup_check_result.txt
+    check_missing_files
     exit 4
 fi
